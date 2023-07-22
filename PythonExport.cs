700166471af6342d204b40107b983be87a1e2ff9
Electrostats <Super_justy@hotmail.com>
2023-07-22 16:35:33 -0400

    Preparing PythonExport

---
+++ PythonExport.cs
@@ -0,0 +1,40 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TootTallyDifficultyCalculator2._0
+{
+    public class PythonExport
+    {
+        [DllExport("LoadChart", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
+        public static Chart LoadChart(string path)
+        {
+            return ChartReader.LoadChart(path);
+        }
+
+        [DllExport("GetChartRatings", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
+        public static double[][] GetRatings(string path)
+        {
+            
+            var chart = ChartReader.LoadChart(path);
+            double[][] ratings = new double[7][];
+            for (int i = 0; i < 7; i++)
+                ratings[i] = new double[]
+                {
+                    chart.GetStarRating(i),
+                    chart.GetAimPerformance(i),
+                    chart.GetTapPerformance(i),
+                    chart.GetAccPerformance(i),
+                };
+
+            return ratings;
+        }
+
+
+
+    }
+}
